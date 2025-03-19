fe052da9734cfde852d934283d2e90ef4151f43b
gccwhu <2998696545@qq.com>
2025-03-19 18:11:21 +0800

    第四次作业

---
+++ assignment4/Clock/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace assignment4
+{
+    // 闹钟类
+    public class AlarmClock
+    {
+        private DateTime _alarmTime;
+        private bool _isAlarmTriggered;
+
+        // 定义事件委托
+        public event EventHandler Tick;
+        public event EventHandler Alarm;
+
+        // 闹钟时间属性
+        public DateTime AlarmTime
+        {
+            get => _alarmTime;
+            set
+            {
+                _alarmTime = value;
+                _isAlarmTriggered = false; // 重置触发状态
+            }
+        }
+
+        // 当前模拟时间
+        public DateTime CurrentTime { get; private set; }
+
+        // 启动闹钟
+        public void Start()
+        {
+            CurrentTime = DateTime.Now;
+            Console.WriteLine($"闹钟启动，当前时间：{CurrentTime:HH:mm:ss}");
+
+            while (true)
+            {
+                // 模拟每秒前进一次
+                Thread.Sleep(1000);
+                CurrentTime = CurrentTime.AddSeconds(1);
+
+                // 触发嘀嗒事件
+                OnTick();
+
+                // 检查是否到达闹钟时间
+                if (!_isAlarmTriggered && CurrentTime >= AlarmTime)
+                {
+                    OnAlarm();
+                    _isAlarmTriggered = true;
+                }
+            }
+        }
+
+        // 触发Tick事件
+        protected virtual void OnTick()
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+        }
+
+        // 触发Alarm事件
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    class clock
+    {
+        static void Main()
+        {
+            var alarmClock = new AlarmClock();
+
+            // 设置5秒后响铃
+            alarmClock.AlarmTime = DateTime.Now.AddSeconds(5);
+
+            // 订阅嘀嗒事件
+            alarmClock.Tick += (sender, e) =>
+            {
+                Console.WriteLine($"[滴答] 当前时间：{alarmClock.CurrentTime:HH:mm:ss}");
+            };
+            // 订阅响铃事件
+            alarmClock.Alarm += (sender, e) =>
+            {
+                Console.WriteLine("[铃声] 叮铃铃！！！");
+            };
+
+            // 启动闹钟
+            alarmClock.Start();
+        }
+    }
+}
+
---
+++ assignment4/Nodelist.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    public class node<T>
+    {
+        public T data { get; set; }
+        public node<T> next { get; set; }   
+        public node(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }   
+    public class GenericList<T>: node<T>
+    {
+        public node<T> Head;
+        public GenericList() : base(default(T)) 
+        {
+            Head = null;
+        }
+        public void Add(T data)
+        {
+            node<T> newNode = new node<T>(data);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                node<T> temp = Head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+        }   
+        public void ForEach(Action<T> action)
+        {
+            node<T> temp = Head;
+            while (temp != null)
+            {
+                action(temp.data);
+                temp = temp.next;
+            }
+        }   
+
+    }   
+    class Nodelist
+    {
+        static void Main(string[] args)
+        {
+            Random  random = new Random();
+            GenericList<double> NodeList = new GenericList<double>();
+            //随机生成10个数  
+            for (int i = 0; i < 10; i++)
+                NodeList.Add(10*random.NextDouble());
+            //打印链表
+            NodeList.ForEach(m=>Console.WriteLine(m));
+            //求和
+            double sum = 0;
+            NodeList.ForEach(m=>sum+=m);
+            Console.WriteLine("Sum of the list is: " + sum);
+            //求最大值
+            double max = 0;
+            NodeList.ForEach(m => max = Math.Max(max, m));
+            Console.WriteLine("Max of the list is: " + max);
+            //求最小值  
+            double min = 32768;    
+            NodeList.ForEach(m => min = Math.Min(min, m));
+            Console.WriteLine("Min of the list is: " + min);    
+        }
+    }
+}
